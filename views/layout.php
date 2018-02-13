91dae85b812949dacdfcf19c6d5edf22f411b1ea
Trung Hiếu Lê <hieult1409@gmail.com>
2018-02-13 18:35:34 +0200

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -8,7 +8,7 @@
 	<head>
 		<title>Image Share</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/readable/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="<?php echo relative_path() ?>assets/css/style.css">
 	</head>
